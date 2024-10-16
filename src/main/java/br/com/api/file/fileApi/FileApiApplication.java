b9137a1d36e287c4c26f395b93b40399523859dc
Rayner Santana <raynersantana@plusoft.com>
2024-10-16 16:03:22 -0300

    Uploading project

---
+++ src/main/java/br/com/api/file/fileApi/FileApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.file.fileApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/api/file/fileApi/FileApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api.file.fileApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
